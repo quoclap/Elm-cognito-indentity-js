787c901e369ab8817a27fd761bc114794f66b95f
QuocLap Tran <quoclap.tran@gmail.com>
2016-11-08 11:53:47 +0700

    Tiep theo sau khi signup, nhan duoc ma confirm gui toi ph, dung de chay Use case 2, thanh cong.

--- src/entry.js
+++ src/entry.js
@@ -1,41 +1,64 @@
 console.log("Hello from entry.js");
 
+//Instructions here https://github.com/aws/amazon-cognito-identity-js/blob/master/README.md
+
 var config = require('./config.js')
 
 // Modules, e.g. Webpack:
 var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 
-//Use case 1
-var poolData = {
-    UserPoolId : config.userPoolId, // Your user pool id here
-    ClientId : config.userPoolClientid // Your client id here
-};
-var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+// //Use case 1
+// var poolData = {
+//     UserPoolId : config.userPoolId, // Your user pool id here
+//     ClientId : config.userPoolClientid // Your client id here
+// };
+// var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+//
+// var attributeList = [];
+//
+// var dataEmail = {
+//     Name : 'email',
+//     Value : config.email
+// };
+//
+// var dataPhoneNumber = {
+//     Name : 'phone_number',
+//     Value : config.phoneNumber
+// };
+// var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
+// var attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute(dataPhoneNumber);
+//
+// attributeList.push(attributeEmail);
+// attributeList.push(attributePhoneNumber);
+//
+// userPool.signUp('quoclap', 'phuonghai123', attributeList, null, function(err, result){
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//   var cognitoUser = result.user;
+//     console.log('user name is ' + cognitoUser.getUsername());
+// });
+//**************************************************************************************
 
-var attributeList = [];
+//Use case 2
+    var poolData = {
+        UserPoolId : config.userPoolId, // Your user pool id here
+        ClientId : config.userPoolClientid  // Your client id here
+    };
 
-var dataEmail = {
-    Name : 'email',
-    Value : config.email
-};
+    var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+    var userData = {
+        Username : 'quoclap',
+        Pool : userPool
+    };
 
-var dataPhoneNumber = {
-    Name : 'phone_number',
-    Value : config.phoneNumber
-};
-var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
-var attributePhoneNumber = new AmazonCognitoIdentity.CognitoUserAttribute(dataPhoneNumber);
-
-attributeList.push(attributeEmail);
-attributeList.push(attributePhoneNumber);
-
-userPool.signUp('quoclap', 'phuonghai123', attributeList, null, function(err, result){
-    if (err) {
-      console.log("bi loi roi!");
-        console.log(err);
-        return;
-    }
-  var cognitoUser = result.user;
-    console.log('user name is ' + cognitoUser.getUsername());
-});
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    cognitoUser.confirmRegistration('977349', true, function(err, result) {
+        if (err) {
+            alert(err);
+            return;
+        }
+        console.log('call result: ' + result);
+    });
